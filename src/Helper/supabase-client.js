1788e520dc91ffe38cd6721551b7b28deafd642f
mennaomar777 <mennaomar764@gmail.com>
2025-10-24 23:36:06 +0300

    Update supabase client

--- src/Helper/supabase-client.js
+++ src/Helper/supabase-client.js
@@ -1,6 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  // eslint-disable-next-line no-undef
+  (typeof process !== "undefined" ? process.env.SUPABASE_URL : "");
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_KEY ||
+  // eslint-disable-next-line no-undef
+  (typeof process !== "undefined" ? process.env.SUPABASE_KEY : "");
+
+if (typeof process === "undefined") {
+  globalThis.process = { env: {} };
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
