c570cc98570c8f7ec70dc2857b6f0f4253fa538e
mennaomar777 <mennaomar764@gmail.com>
2025-10-24 23:57:19 +0300

    edit

--- src/Helper/supabase-client.js
+++ src/Helper/supabase-client.js
@@ -1,19 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
-  // eslint-disable-next-line no-undef
-  (typeof process !== "undefined" ? process.env.SUPABASE_URL : "");
-const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_KEY ||
-  // eslint-disable-next-line no-undef
-  (typeof process !== "undefined" ? process.env.SUPABASE_KEY : "");
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
-if (typeof process === "undefined") {
-  globalThis.process = { env: {} };
-}
-
-console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("Supabase KEY:", import.meta.env.VITE_SUPABASE_KEY);
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase KEY:", supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
